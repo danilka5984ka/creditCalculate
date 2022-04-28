994d2ca5b1e0c4f18abd1a437e5185aee17206c1
Daniil Peresumkin <danilka5984ka@yandex.ru>
2022-04-28 18:45:32 +0700

    reCreditCalculate

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -4,25 +4,7 @@ public class CreditPaymentService {
 
         int months = years * 12;
         int payment = amount / months;
-        System.out.println("Ежемесячный платёжь за 1 год составит: " + payment);
-        return payment;
-
-    }
-
-    public int calculate1(int amount, int years) {
-
-        int months = years * 12;
-        int payment = amount / months;
-        System.out.println("Ежемесячный платёжь за 2 года составит: " + payment);
-        return payment;
-
-    }
-
-    public int calculate2(int amount, int years) {
-
-        int months = years * 12;
-        int payment = amount / months;
-        System.out.println("Ежемесячный платёжь за 3 года составит: " + payment);
+        System.out.println("Ежемесячный платёжь за указанный период составит: " + payment);
         return payment;
 
     }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        service.calculate(1_000_000, 1);
-        service.calculate1(1_000_000, 2);
-        service.calculate1(1_000_000, 3);
+        service.calculate(1_000_000, 3);
+
     }
 }
